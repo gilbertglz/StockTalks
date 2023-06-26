0287eb3791318576af85a00e5307f15e5fb44648
Gilbert Gonzalez <gilbertgonzalez53@gmail.com>
2023-06-26 17:26:57 -0500

    Added CSV files

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sb
+import seaborn as sns
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -11,12 +11,14 @@ if __name__ == '__main__':
     delcols = ["Dividends", "Stock Splits", "Volume"]
     for a in delcols:
         del MSOFT[a]
-    #plt.plot(MSOFT["Open"])
-    #plt.show()
-    print(MSOFT)
-    MSOFT["Open"].plot()
+    # plt.plot(MSOFT["Open"])
+    # plt.show()
+    # print(MSOFT)
+    # print(MSOFT.count())
+    sumSoft = MSOFT.head()
+    normSoft = sumSoft.div(sumSoft.sum(axis="columns"), axis="index")
+    print(normSoft)
+    normSoft[["Close", "Open"]].plot.bar(stacked=True)
+    # sns.barplot(x="Open", y="Close", data=MSOFT.head(), orient="h")
+    # MSOFT["Open"].plot()
     plt.show()
-
-
-
-
