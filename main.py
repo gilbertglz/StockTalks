880c6a977da3dc4fd83cf326763f8f4d5949a3da
Gilbert Gonzalez <gilbertgonzalez53@gmail.com>
2023-07-18 04:03:52 -0600

    feat(MFool): Scraping The Motley Fool Investment News to analyze how often they're correct

--- main.py
+++ main.py
@@ -30,8 +30,7 @@ def find_company_ticker(company):
         return ""
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+def redditScrape():
     pd.set_option("display.max_columns", None)
     pd.set_option("display.max_rows", None)
 
@@ -108,3 +107,42 @@ if __name__ == '__main__':
     df['Symbols'] = companySymbols
     print(df.to_string)
     df.to_csv("file.csv")
+
+
+def motleyMultiplePages(soup):
+    Titles, Authors, authorClean, Dates = [], [], [], []
+    for tag in soup.find_all('h5', attrs="self-center mb-6 font-medium md:text-h5 text-md md:mb-4px"):
+        Titles.append(tag.get_text())
+    for tags in soup.find_all('div', attrs="text-sm text-gray-800 mb-2px md:mb-8px"):
+        date, author = [x for x in (tags.text.split('\n')) if x.strip() != '']
+        Authors.append(author)
+        Dates.append(date)
+    for author in Authors:
+        authorClean.append([x for x in (author.split('by ')) if x.strip() != ''])
+    out = zip(Titles, authorClean, Dates)
+    return out
+
+def motleyFoolScrape():
+    url = "https://www.fool.com/investing-news/"
+    pages = 5
+    Titles, authorClean, Dates = [], [], []
+    for i in range(1, pages + 1):
+        ss = requests.get(url + "?page=" + i.__str__())
+        data = ss.text
+        soup = bs(data, 'html.parser')
+        resultMotley = motleyMultiplePages(soup)
+        for item1, item2, item3 in resultMotley:
+            Titles.append(item1)
+            authorClean.append(item2)
+            Dates.append(item3)
+    Table = pd.DataFrame()
+    Table['Titles'] = Titles
+    Table['Author'] = authorClean
+    Table['Date'] = Dates
+    Table = Table.drop_duplicates(subset=['Titles'])
+    print(Table)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    motleyFoolScrape()
